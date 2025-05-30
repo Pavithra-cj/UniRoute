cacaa1858638f4141e6625ee01af7d84ecb87e9a
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2025-05-30 11:28:42 +0530

    Initial Commit

---
+++ UniRoute/UniRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniRouteApp.swift
+//  UniRoute
+//
+//  Created by Pavithra Chamod on 2025-05-30.
+//
+
+import SwiftUI
+
+@main
+struct UniRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniRouteTests/UniRouteTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniRouteTests.swift
+//  UniRouteTests
+//
+//  Created by Pavithra Chamod on 2025-05-30.
+//
+
+import Testing
+@testable import UniRoute
+
+struct UniRouteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
